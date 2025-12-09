d7c9020e8a81fd3228b3de137ea75c9b98eb8465
php56611-ship-it <php56611@gmail.com>
2025-12-08 21:57:34 -0300

    Adicionado teste à whitelist - 2025-12-08 21:56:43

--- Whitelist.lua
+++ Whitelist.lua
@@ -78,6 +78,7 @@ local WhitelistData = {
     ["ensisbsjbrhe"] = {type = "Usuário ADM", expires = parseDateTime("09/10/2030 19:40")},
     ["gui_neh1023"] = {type = "Usuário ADM", expires = parseDateTime("09/10/2030 19:40")},
     ["killert_494"] = {type = "Usuário ADM", expires = parseDateTime("09/10/2030 19:40")},
+    ["teste"] = {type = "Usuário ADM", expires = nil},
 }
 
 -- Função para verificar se a whitelist está expirada
